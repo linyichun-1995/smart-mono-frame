1ba0d1eb36e71b252534f42c5caa6b3f7d90ddf0
lin <luluaalululu42@gmail.com>
2025-05-02 19:50:55 +0800

    chore: 初始化项目配置并添加开发工具
    添加了.npmrc、.prettierrc.json、pnpm-workspace.yaml、eslint.config.js和.vscode/settings.json等配置文件，并更新了package.json中的依赖项和脚本。这些更改旨在为项目提供一致的开发环境、代码格式化规则和包管理配置。

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import eslint from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import prettierPlugin from "eslint-plugin-prettier"
+import eslintPluginVue from "eslint-plugin-vue"
+import globals from "globals"
+import typescriptEslint from "typescript-eslint"
+import vueParser from "vue-eslint-parser"
+
+export default typescriptEslint.config(
+  { ignores: ["**/node_modules/", "**/dist/", "**/build/", "**/coverage/"] },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+      ...eslintPluginVue.configs["flat/recommended"],
+    ],
+    files: ["**/*.{ts,vue}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+      parser: vueParser,
+      parserOptions: {
+        parser: typescriptEslint.parser,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier":  "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      // your rules
+    },
+  },
+  eslintConfigPrettier,
+)
